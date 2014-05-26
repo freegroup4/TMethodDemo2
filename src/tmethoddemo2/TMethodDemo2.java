b3de172ea04602261ab4b7328bdf3154593fb016
freegroup4 <freegroup4@yopmail.com>
2014-05-26 21:18:14 +0800

    Final Commit
    Final

--- src/tmethoddemo2/TMethodDemo2.java
+++ src/tmethoddemo2/TMethodDemo2.java
@@ -1,22 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package tmethoddemo2;
-
-/**
- *
- * @author Chen-USE
- */
 public class TMethodDemo2 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int A[] = {1,2,3,4,5};
+        Arrmethod(A);
+        System.out.println("呼叫ArrMethod之後");
+        for (int i = 0; i < A.length; i++) {
+            System.out.println(A[i] +" ");
+        }
     }
-    
+    public static void Arrmethod(int[] A) {
+        int i;
+        for (i = 0; i < A.length; i++) {
+            A[i] = A[i] * A[i];
+        }
+    }
 }
